81a8bed01a0d489d9c7f589ae4465a9ed82e1402
Naomi Veroczi <nbveroczi@Naomis-MacBook-Pro.local>
2016-04-06 22:24:14 -0700

    edit first and second file of project

--- discover_more_python/github_most_popular.py
+++ discover_more_python/github_most_popular.py
@@ -1,9 +1,9 @@
+import urllib2
+
 request_headers = {
   'User-Agent': 'Holberton_School',
   'Authorization': 'token 42ee520bd99c8951da18894d2e0cd0c06d957089'
 } 
-
-import urllib2
 
 def main():
   # open a connection to a URL using urllib2
